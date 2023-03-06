b469247ebbe6ee606c534b4132e54eacf92e6a13
tech-nirav <21it1@svitvasad.ac.in>
2023-03-06 13:55:00 +0530

    Just started setup

---
+++ src/components/weather.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const weather = () => {
+  return (
+    <>
+    <div className="wrap">
+        <div className="search"></div>
+    </div>
+    </>
+  )
+}
+
+export default weather
